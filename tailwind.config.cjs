bab0447c0126fa7696b2c08866a787c6899c3cb3
Saurabh <60008525+dabbygabby@users.noreply.github.com>
2022-04-13 22:43:29 +0530

    new theme tested for react tailwind ui

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,22 +1,36 @@
 module.exports = {
   content: [
     './src/**/*.{js,jsx,ts,tsx}',
-    './.storybook/**/*.{js,jsx,ts,tsx,mdx}'
+    './.storybook/**/*.{js,jsx,ts,tsx,mdx}',
   ],
   safelist: [
     {
-      pattern: /./
+      pattern: /./,
     },
   ],
   daisyui: {
     styled: true,
-    themes: true,
+    themes: [
+      {
+        ironMaiden: {
+          primary: '#006FFF',
+          secondary: '#595F72',
+          accent: '#F68A51',
+          neutral: '#1a2d3c',
+          'base-100': '#011627',
+          info: '#2F89FF',
+          success: '#0FFF95',
+          warning: '#F2AF29',
+          error: '#DB504A',
+        },
+      },
+    ],
     base: true,
     utils: true,
     logs: true,
     rtl: false,
-    prefix: "",
-    darkTheme: "dark",
+    prefix: '',
+    darkTheme: 'dark',
   },
-  plugins: [require("daisyui")],
+  plugins: [require('daisyui')],
 }
