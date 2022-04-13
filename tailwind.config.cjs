2cd01a2b3e79198b3cc089a06ce245473152c602
Saurabh <60008525+dabbygabby@users.noreply.github.com>
2022-04-13 23:12:53 +0530

    added more stuff git push

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,21 +10,7 @@ module.exports = {
   ],
   daisyui: {
     styled: true,
-    themes: [
-      {
-        ironMaiden: {
-          primary: '#006FFF',
-          secondary: '#595F72',
-          accent: '#F68A51',
-          neutral: '#1a2d3c',
-          'base-100': '#011627',
-          info: '#2F89FF',
-          success: '#0FFF95',
-          warning: '#F2AF29',
-          error: '#DB504A',
-        },
-      },
-    ],
+    themes: true,
     base: true,
     utils: true,
     logs: true,
